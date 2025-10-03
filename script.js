dee9e6f01544578422fe8bcc8f980d13601af490
Sean Mafnas <seanvincent671@gmail.com>
2025-10-03 22:09:20 +1000

    Refactor search form and update JavaScript for improved functionality and styling

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+const BASE_URL = "https://www.omdbapi.com/";
+const API_KEY = "d7ad715c";
+const searchForm = document.querySelector(".search__form");
+const searchBar = document.getElementById("search-bar");
+
 async function getMovieDatabase() {
   const response = await fetch(
-    "https://www.omdbapi.com/?apikey=d7ad715c&t=Guardians_of_the_galaxy_vol._2"
+    `${BASE_URL}?apikey=${API_KEY}&s=${searchBar.value}`
   );
   const data = await response.json();
   console.log(data);
+
+  // STOPPED HERE: NEED TO FIGURE OUT HOW TO RENDER
+  // OUT THE TITLES IN THE ARRAY
 }
-getMovieDatabase();
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  getMovieDatabase();
+  console.log("hello!");
+  console.log(searchBar.value);
+};
+
+searchForm.addEventListener("submit", handleSubmit);
